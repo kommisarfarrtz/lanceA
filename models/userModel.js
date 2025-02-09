d56d07f93e7e2f4269bb29ba455e89dce65cc370
rayhana albouchi <rayhanaalbouchi@gmail.com>
2025-02-09 02:47:00 +0000

    modelupdate

--- models/userModel.js
+++ models/userModel.js
@@ -7,7 +7,7 @@ exports.getUserById = (id, callback) => {
   db.query("SELECT * FROM users WHERE id=?", [id], callback);
 }
 exports.createUser = (user, callback) => {
-  db.query("INSERT INTO users (name, age, email) VALUES (?, ?, ?)", [user.name, user.age, user.email], callback);
+  db.query("INSERT INTO users (name,lastName,email,password,profile_picture,location,is_seller,stars) VALUES (?, ?,?,?,?,?,?,?)", [user.name,user.lastName, user.email,user.password,user.profile_picture,user.location,user.is_seller,user.stars], callback);
 }
 exports.updateUser = (id, user, callback) => {
   db.query("UPDATE users SET name=?, age=?, email=? WHERE id=?", [user.name, user.age, user.email, id], callback);
