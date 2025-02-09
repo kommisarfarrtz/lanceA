0bbe09d3547a407c25db1aea023b937f6a07c943
Santz47 <fedysalah987@gmail.com>
2025-02-09 14:47:17 +0100

    bombalajddd

--- models/userModel.js
+++ models/userModel.js
@@ -9,10 +9,13 @@ exports.getUserById = (id, callback) => {
 }
 
 exports.createUser = (user, callback) => {
-  db.query("INSERT INTO users (name, email) VALUES (?, ?)", [user.name, user.email], callback);
+  db.query(
+    "INSERT INTO users (name, lastName, email, password, profile_picture, location, is_seller, stars) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+    [user.name,user.lastName,user.email,user.password,user.profile],callback);
 }
 
 exports.updateUser = (id, user, callback) => {
+
   db.query("UPDATE users SET name=?, age=?, email=? WHERE id=?", [user.name, user.age, user.email, id], callback);
 }
 
