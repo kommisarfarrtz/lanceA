7586003af31477dcc0ea9d914aac7bd52c14192f
rayhana albouchi <rayhanaalbouchi@gmail.com>
2025-02-10 22:26:23 +0000

    c

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -15,12 +15,6 @@ exports.createService = (req, res) => {
 exports.deleteService = (req, res) => {
     servicesModel.deleteService(req, res);
 }
-<<<<<<< HEAD
-
-exports.createCompletedService = (req, res) => {
-    servicesModel.createCompletedService(req, res);
-=======
 exports.completedServices = (req, res) => {
     servicesModel.completedServices(req, res);
->>>>>>> 63f0c3f3f1b552cc22be3a27532b5fb63f754bf1
 }
