b6f35206db4f327975a781ea963e3ae5f18539e0
Santz47 <fedysalah987@gmail.com>
2025-02-11 14:16:43 +0100

    adminFix

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const router=express.Router()
-const adminController=require("../controllers/adminController.js")
 const userController=require("../controllers/userController.js")
 const servicesController=require("../controllers/servicesController.js")
 
 router.get("users", userController.getUsers);
 router.get("/services", servicesController.getServices);
-router.delete("/services/:id", servicesControllerController.deleteService);
-router.get("/category", adminController.getServicesByCategory);
-router.post("/category", adminController.createCategory);
-router.delete("/category/:id", adminController.deleteCategory);
+router.delete("/services/:id", servicesController.deleteService);
+router.post("/category", servicesController.createCategory);
+router.delete("/category/:id", servicesController.deleteCategory);
 
+module.exports= router
